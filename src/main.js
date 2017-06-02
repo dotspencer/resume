b67235e722dc0f4f4ab8c79737b03839829f4c70
Spencer Smith <spencer.smith@ldschurch.org>
2017-06-02 13:10:07 -0600

    Starting header

--- src/main.js
+++ src/main.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import styles from './common.scss';
+import Header from './components/header';
 
 class App extends React.Component {
+  componentDidMount(){
+    var info = require(__dirname + '/data/info.json');
+    console.log(info);
+  }
+
   render(){
     return (
-      <h1>Smith</h1>
+      <div>
+        <Header name={5 + 5}/>
+      </div>
     );
   }
 }
